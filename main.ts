09724c082b1f6d9b36c83175c4b9e8454b53fc2e
crazycardriverZ <123813665+crazycardriverZ@users.noreply.github.com>
2023-03-25 12:09:54 +0000

    Practically same thing xD

--- main.ts
+++ main.ts
@@ -21,11 +21,11 @@ input.onButtonPressed(Button.A, function () {
     basic.pause(100)
     music.playTone(440, music.beat(BeatFraction.Half))
     basic.pause(100)
-    music.playTone(494, music.beat(BeatFraction.Quarter))
+    music.playTone(494, music.beat(BeatFraction.Eighth))
     basic.pause(100)
     music.playTone(466, music.beat(BeatFraction.Quarter))
     basic.pause(100)
-    music.playTone(440, music.beat(BeatFraction.Half))
+    music.playTone(784, music.beat(BeatFraction.Half))
 })
 basic.forever(function () {
 	
